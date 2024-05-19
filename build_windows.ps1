3e340da1e09886c0e22244f223bdfb919e6810aa
Ed_ <edwardgz@gmail.com>
2024-05-19 13:47:57 -0400

    more small fixes to build_windows.ps1

--- build_windows.ps1
+++ build_windows.ps1
@@ -4,8 +4,8 @@ $devshell = Join-Path $PSScriptRoot 'devshell.ps1'
 $path_sokol = Join-Path $PSScriptRoot 'sokol'
 if (test-path $path_sokol)
 {
-	Move-Item -Path "$path_sokol/*" -Destination $PSScriptRoot -Force
-	Remove-Item -Path $path_sokol -Recusre -Force
+	Move-Item   -Path "$path_sokol/*" -Destination $PSScriptRoot -Force
+	Remove-Item -Path $path_sokol -Recurse -Force
 }
 
-& 'build_clibs_windows.cmd'
+& '.\build_clibs_windows.cmd'
