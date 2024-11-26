17f2d54481630380fdb4b764bc0ee66af495f46a
Ed_ <edwardgz@gmail.com>
2024-11-26 10:29:57 -0500

    update sokol_gp

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,11 +1,13 @@
 $devshell = Join-Path $PSScriptRoot 'devshell.ps1'
 & $devshell -arch amd64
 
-$path_sokol = Join-Path $PSScriptRoot 'sokol'
+$path_sokol    = Join-Path $PSScriptRoot 'sokol'
+$path_examples = join-path $PSScriptRoot 'examples'
 if (test-path $path_sokol)
 {
 	Move-Item   -Path "$path_sokol/*" -Destination $PSScriptRoot -Force
-	Remove-Item -Path $path_sokol -Recurse -Force
+	Remove-Item -Path $path_sokol    -Recurse -Force
+	Remove-Item -Path $path_examples -Recurse -Force
 }
 
 & '.\build_clibs_windows.cmd'
